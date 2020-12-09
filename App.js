58a3e583b1e9e7a2772fbff260e3b32d5b1da27d
Oleksii <39428663+adovg@users.noreply.github.com>
2020-12-09 14:41:03 +0200

    todo btn

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Navbar} from './src/Navbar';
+import {AddTodo} from './src/AddTodo';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Navbar title="Todo App"/>
+      <AddTodo/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/AddTodo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, StyleSheet, TextInput, Button} from 'react-native';
+
+export const AddTodo = (props) => {
+    return (
+        <View style = {styles.block}>
+            <TextInput />
+            <Button title='Добавить' />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    block: {
+        flexDirection: 'row',
+        marginTop: '10'
+    },
+    input: {
+        width: '80%'
+    }
+})
